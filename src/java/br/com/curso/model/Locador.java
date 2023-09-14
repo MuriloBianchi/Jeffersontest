82e549fff121047391e7db83dd9f4d435f9fe0a1
MuriloBianchi <murilolopes.b@gmail.com>
2023-09-14 20:38:33 -0300

    models2 e daoTipoImovel

---
+++ src/java/br/com/curso/model/Locador.java
@@ -0,0 +1,47 @@
+package br.com.curso.model;
+
+public class Locador {
+    private int idLocador;
+    private String nome;
+    private String cpfCnpj;
+
+    public Locador() {
+        idLocador = 0;
+        nome = "";
+        cpfCnpj = "";
+    }
+
+    public Locador(int idLocador, String nome, String cpfCnpj) {
+        this.idLocador = idLocador;
+        this.nome = nome;
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public int getIdLocador() {
+        return idLocador;
+    }
+
+    public void setIdLocador(int idLocador) {
+        this.idLocador = idLocador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+    
+    
+    
+    
+}
