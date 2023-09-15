99e825ecc2d6c5209288bfadbfb6667dd9089b73
Kelvin Freitas <kelvinfreitas2004@gmail.com>
2023-09-14 21:22:02 -0300

    Subindo algumas DAO

---
+++ src/java/br/com/curso/dao/LocatarioDAO.java
@@ -0,0 +1,193 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.curso.dao;
+
+import br.com.curso.model.Locatario;
+import br.com.curso.utils.SingleConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author kelvi
+ */
+public class LocatarioDAO implements GenericDAO {
+       private Connection conexao;
+    
+    public LocatarioDAO() throws Exception{
+        conexao = SingleConnection.getConnection();
+    }
+    
+    @Override
+    public Boolean cadastrar(Object objeto) {
+      Locatario oLocatario = (Locatario) objeto;
+      boolean retorno = false;
+      if(oLocatario.getIdLocatario() == 0){
+          retorno = inserir(oLocatario);
+    } else{
+          retorno = alterar(oLocatario);
+      }
+      return retorno;
+    }
+
+    @Override
+    public Boolean inserir(Object objeto) {
+      Locatario oLocatario = (Locatario)objeto;
+        PreparedStatement stmt = null;
+        String sql = "Insert into Locatario ( nome, cpfcnpj) values (?,?)";
+        try{
+          stmt = conexao.prepareStatement(sql);
+          stmt.setString(1, oLocatario.getNome());
+          stmt.setString(2, oLocatario.getCpfCnpj());
+          stmt.execute();
+          conexao.commit();
+          return true;
+        }catch (Exception ex){
+            try{
+            System.out.println("Problemas ao cadastrar a Locatario! Erro:" + ex.getMessage());
+            ex.printStackTrace();
+              conexao.rollback();
+          }catch(SQLException e){
+                System.out.println("Erro"+ e.getMessage());
+                e.printStackTrace();
+          }
+            return false;
+       }
+    }
+
+    @Override
+    public Boolean alterar(Object objeto) {
+       Locatario oLocatario = (Locatario)objeto;    
+        PreparedStatement stmt = null;
+        String sql = "update locatario set nome=?, cpfcnpj=? where idlocatario=?";
+        try{
+          stmt = conexao.prepareStatement(sql);
+          stmt.setString(1, oLocatario.getNome());
+          stmt.setString(2, oLocatario.getCpfCnpj());
+          stmt.setInt(3, oLocatario.getIdLocatario());
+          stmt.execute();
+          conexao.commit();
+          return  true;
+        } catch (Exception ex){
+            try{
+                System.out.println("Problemas ao alterar a Locatario! Erro:" +ex.getMessage());
+                ex.printStackTrace();
+                conexao.rollback();
+            }catch (SQLException e){
+                System.out.println("Erro:" +e.getMessage());
+                e.printStackTrace();
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public Boolean excluir(int numero) {
+         int idLocatario = numero;
+        PreparedStatement stmt = null;
+        String sql = "delete from locatario where idlocatario=?";
+        try{
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, idLocatario);
+            stmt.execute();
+            conexao.commit ();
+            return true;
+        }catch (Exception e){
+            try{
+                System.out.println("Problemas ao excluir Locatario!erro: " + e.getMessage());
+                e.printStackTrace();
+                conexao.rollback();
+            }catch (SQLException ex){
+                System.out.println("Problemas ao executar rollback" + ex.getMessage());
+                ex.printStackTrace();
+            }
+            return false;
+        }
+    }
+    
+
+    @Override
+    public Object carregar(int numero) {
+        int idLocatario = numero;
+        PreparedStatement stmt = null; 
+        ResultSet rs = null;
+        Locatario oLocatario = null;
+        String sql = "Select * from locatario where idLocatario = ?";
+        try{
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, idLocatario);
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                oLocatario = new Locatario();
+                oLocatario.setIdLocatario(rs.getInt("idLocatario"));
+                oLocatario.setNome(rs.getString("nome"));
+                oLocatario.setCpfCnpj(rs.getString("cpfCnpj"));
+                  
+            }
+        }catch (Exception e){
+            System.out.println("Problemas ao carregar Locatario! Erro: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return oLocatario;
+    }
+
+    @Override
+    public List<Object> listar() {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Object> resultado = new ArrayList<>();
+        Locatario oLocatario = null;
+        String sql = "select * from  Locatario";
+         try{
+             stmt = conexao.prepareStatement(sql);
+             rs = stmt.executeQuery();
+             while(rs.next()){
+                oLocatario = new Locatario();
+                oLocatario.setIdLocatario(rs.getInt("idLocatario"));
+                oLocatario.setNome(rs.getString("nome"));
+                 oLocatario.setCpfCnpj(rs.getString("cpfCnpj"));
+                resultado.add(oLocatario);               
+             }
+         }catch(Exception e){
+             System.out.println("Porblemas ao listar Locatario! Erro: " + e.getMessage());
+             e.printStackTrace();
+         }
+          return resultado;
+    }
+    
+    
+    public String listarJSON(){
+        String strJson="";
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Object> resultado = new ArrayList<>();
+        Locatario Locatario = null;
+        String sql = "select * from locatario";
+        try{
+            stmt = conexao.prepareStatement(sql);
+            rs = stmt.executeQuery();
+            strJson = "[";
+            int i = 0;
+            while(rs.next()){
+                if (i>0) strJson+=",";
+                strJson += "{\"idLocatario\"}:"+rs.getInt("idlocatario")+","
+                        + "\"nome\":\""+rs.getString("nome")+"\","
+                        + "\"cpfCnpj\":\""+rs.getString("cpfcnpj")+"\",";
+                
+                i++;             
+            }
+            strJson += "]";
+        }catch (Exception e){
+            System.out.println("Problemas ao listar locatario! Erro:" + e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println(strJson);
+        return strJson;
+    }
+}
---
+++ src/java/br/com/curso/dao/TipoDescontoDAO.java
@@ -0,0 +1,191 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.curso.dao;
+
+
+import br.com.curso.model.TipoDesconto;
+import br.com.curso.utils.SingleConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author kelvi
+ */
+public class TipoDescontoDAO implements GenericDAO {
+     private Connection conexao;
+    
+    public TipoDescontoDAO() throws Exception{
+        conexao = SingleConnection.getConnection();
+    }
+    
+    @Override
+    public Boolean cadastrar(Object objeto) {
+      TipoDesconto oTipoDesconto = (TipoDesconto) objeto;
+      boolean retorno = false;
+      if(oTipoDesconto.getIdTipoDesconto() == 0){
+          retorno = inserir(oTipoDesconto);
+    } else{
+          retorno = alterar(oTipoDesconto);
+      }
+      return retorno;
+    }
+
+    @Override
+    public Boolean inserir(Object objeto) {
+      TipoDesconto oTipoDesconto = (TipoDesconto)objeto;
+        PreparedStatement stmt = null;
+        String sql = "Insert into TipoDesconto (descricao) values (?)";
+        try{
+          stmt = conexao.prepareStatement(sql);
+          stmt.setString(1, oTipoDesconto.getDescricao());
+          stmt.execute();
+          conexao.commit();
+          return true;
+        }catch (Exception ex){
+            try{
+            System.out.println("Problemas ao cadastrar a TipoDesconto! Erro:" + ex.getMessage());
+            ex.printStackTrace();
+              conexao.rollback();
+          }catch(SQLException e){
+                System.out.println("Erro"+ e.getMessage());
+                e.printStackTrace();
+          }
+            return false;
+       }
+    }
+
+    @Override
+    public Boolean alterar(Object objeto) {
+       TipoDesconto oTipoDesconto = (TipoDesconto)objeto;    
+        PreparedStatement stmt = null;
+        String sql = "update tipoDesconto set descricao=? where idtipodesconto=?";
+        try{
+          stmt = conexao.prepareStatement(sql);
+          stmt.setString(1, oTipoDesconto.getDescricao());
+          stmt.setInt(2, oTipoDesconto.getIdTipoDesconto());
+          stmt.execute();
+          conexao.commit();
+          return  true;
+        } catch (Exception ex){
+            try{
+                System.out.println("Problemas ao alterar a TipoDesconto! Erro:" +ex.getMessage());
+                ex.printStackTrace();
+                conexao.rollback();
+            }catch (SQLException e){
+                System.out.println("Erro:" +e.getMessage());
+                e.printStackTrace();
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public Boolean excluir(int numero) {
+         int idTipoDesconto = numero;
+        PreparedStatement stmt = null;
+        String sql = "delete from tipoDesconto where idtipodesconto=?";
+        try{
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, idTipoDesconto);
+            stmt.execute();
+            conexao.commit ();
+            return true;
+        }catch (Exception e){
+            try{
+                System.out.println("Problemas ao excluir TipoDesconto! erro: " + e.getMessage());
+                e.printStackTrace();
+                conexao.rollback();
+            }catch (SQLException ex){
+                System.out.println("Problemas ao executar rollback" + ex.getMessage());
+                ex.printStackTrace();
+            }
+            return false;
+        }
+    }
+    
+
+    @Override
+    public Object carregar(int numero) {
+         int idTipoDesconto = numero;
+        PreparedStatement stmt = null; 
+        ResultSet rs = null;
+        TipoDesconto oTipoDesconto = null;
+        String sql = "Select * from tipoDesconto where idTipoDesconto = ?";
+        try{
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, idTipoDesconto);
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                oTipoDesconto = new TipoDesconto();
+                oTipoDesconto.setIdTipoDesconto(rs.getInt("idTipoDesconto"));
+                oTipoDesconto.setDescricao(rs.getString("descricao"));
+                
+                  
+            }
+        }catch (Exception e){
+            System.out.println("Problemas ao carregar Locatario! Erro: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return oTipoDesconto;
+    }
+
+    @Override
+    public List<Object> listar() {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Object> resultado = new ArrayList<>();
+        TipoDesconto oTipoDesconto = null;
+        String sql = "select * from TipoDesconto";
+         try{
+             stmt = conexao.prepareStatement(sql);
+             rs = stmt.executeQuery();
+             while(rs.next()){
+                oTipoDesconto = new TipoDesconto();
+                oTipoDesconto.setIdTipoDesconto(rs.getInt("idTipoDesconto"));
+                oTipoDesconto.setDescricao(rs.getString("descricao"));
+                 
+                resultado.add(oTipoDesconto);               
+             }
+         }catch(Exception e){
+             System.out.println("Problemas ao listar TipoDesconto! Erro: " + e.getMessage());
+             e.printStackTrace();
+         }
+          return resultado;
+    }
+    
+    
+    public String listarJSON(){
+        String strJson="";
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Object> resultado = new ArrayList<>();
+        TipoDesconto TipoDesconto = null;
+        String sql = "select * from tipoDesconto";
+        try{
+            stmt = conexao.prepareStatement(sql);
+            rs = stmt.executeQuery();
+            strJson = "[";
+            int i = 0;
+            while(rs.next()){
+                if (i>0) strJson+=",";
+                strJson += "{\"idTipoDesconto\"}:"+rs.getInt("idTipoDesconto")+","
+                        + "\"descricao\":\""+rs.getString("descricao")+"\",";
+                
+                i++;             
+            }
+            strJson += "]";
+        }catch (Exception e){
+            System.out.println("Problemas ao listar TipoDesconto! Erro:" + e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println(strJson);
+        return strJson;
+    }
+}
